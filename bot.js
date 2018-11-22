b8275440ed6181b15dfd91c9e5b01d4c6e5cf3e6
aazddsf <44817527+aazddsf@users.noreply.github.com>
2018-11-22 23:04:27 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,4 +29,17 @@ client.user.setStatus("dnd")
 
 
 
+
+client.on("guildMemberAdd", async member => {
+  let moment2 = require('moment-duration-format'),
+      moment = require("moment"),
+      date = moment.duration(new Date() - member.user.createdAt).format("d");
+
+  if(date <30) {
+    member.ban("Member account age is lower than 7 days.")
+  }
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
